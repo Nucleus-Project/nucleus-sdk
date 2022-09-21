31b8d1b8e15a5794b634e349453e185cf0140a3e
Leopold <bugybug@icloud.com>
2022-09-21 03:31:00 +0300

    Playingcards

--- ixhl2rp/plugins/playingcards/items/containerbags/sh_card_deck.lua
+++ ixhl2rp/plugins/playingcards/items/containerbags/sh_card_deck.lua
@@ -2,7 +2,7 @@
 ITEM.name = "52-Card Deck"
 ITEM.model = Model("models/cards/stack.mdl")
 ITEM.description = "A traditional 52-card deck of French-suited playing cards."
-ITEM.category = "Playing Cards"
+ITEM.category = "Развлечение"
 ITEM.invWidth = 13
 ITEM.invHeight = 4
 ITEM.allowNesting = true
