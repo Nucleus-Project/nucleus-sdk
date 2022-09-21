4fae80862bd9864d5b453e6c2784f2e805dc53c9
Leopold <bugybug@icloud.com>
2022-09-21 03:22:21 +0300

    Books items for localization

---
+++ ixhl2rp/plugins/books/items/books/sh_bookexample.lua
@@ -0,0 +1,44 @@
+ITEM.name = "Book Example"
+
+
+
+ITEM.model = "models/props_lab/bindergreen.mdl"
+
+
+
+ITEM.width = 1
+
+
+
+ITEM.height = 1
+
+
+
+ITEM.description = "It's a book, duh."
+
+
+
+
+
+
+
+ITEM.title = "Some Book"
+
+
+
+ITEM.text = [[
+
+
+
+Hello!
+
+
+
+What did you expect to see here?
+
+
+
+]]
+
+
+
